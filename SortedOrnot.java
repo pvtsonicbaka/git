b2587bf69571492db609ece945af47b71d1b9708
pvtsonicbaka <ashiyanap1680@gmail.com>
2023-10-29 12:35:34 +0530

    293 question

---
+++ SortedOrnot.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+
+class SortedOrnot{
+    
+    public static void main(String[] args) {
+       
+        throw new ArithmeticException("hii");
+       }
+
+    }   
---
+++ T_2_293.java
@@ -0,0 +1,16 @@
+class T_2_293{
+    public static void main(String[] args) {
+        int n=5;
+        int num=1;
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            System.out.println();
+            }
+            
+        }
+ }
