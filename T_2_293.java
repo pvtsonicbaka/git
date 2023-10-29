01965a8a3659904fc81cec8d717480c339ec227a
pvtsonicbaka <ashiyanap1680@gmail.com>
2023-10-29 13:27:52 +0530

    question 293

--- T_2_293.java
+++ T_2_293.java
@@ -1,7 +1,7 @@
 class T_2_293{
     public static void main(String[] args) {
         int n=5;
-        int num=1;
+        
         for(int i=n;i>=1;i--){
             for(int j=1;j<=(n-i);j++){
                 System.out.print(" ");
